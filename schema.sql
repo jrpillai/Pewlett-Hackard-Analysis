285d19d92c04e03459f46691d6925d0f81006755
jrpillai <99375785+jrpillai@users.noreply.github.com>
2022-03-28 23:07:22 -0500

    adding queries file

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	 emp_no INT NOT NULL,
+	emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -33,4 +33,23 @@ CREATE TABLE salaries (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+dept_no VARCHAR(4) NOT NULL,
+emp_no INT NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE Table titles (
+emp_no INT NOT NULL,
+title VARCHAR(40) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+PRIMARY KEY (emp_no, title, from_date)
 );
